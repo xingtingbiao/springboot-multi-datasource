bf69ec237c6055a4677cc1ff2847af17631b5282
xingtingbiao <18551617816@163.com>
2025-02-07 16:18:20 +0800

    Add files via upload

---
+++ springboot-mybatis-demo/src/main/java/com/cfex/mybatis/config/RequestCachingFilter.java
@@ -0,0 +1,25 @@
+package com.cfex.customer.config.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.Generated;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+
+import java.io.IOException;
+
+@Generated
+@Component
+public class RequestCachingFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(@NotNull HttpServletRequest request,
+                                    @NotNull HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        filterChain.doFilter(new ContentCachingRequestWrapper(request), response);
+    }
+}
